2623d38c2aa1edbbdb50af68ece26bd838761eb5
cothi <jiungdev@gmail.com>
2022-11-03 23:35:13 +0900

    feat: complete layout of validator page, but no data connection yet

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,23 +1,23 @@
-const CracoLessPlugin = require("craco-less");
-const { getThemeVariables } = require("antd/dist/theme");
+const CracoLessPlugin = require('craco-less')
+const { getThemeVariables } = require('antd/dist/theme')
 
 module.exports = {
-  plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            javascriptEnabled: true,
-            modifyVars: {
-              ...getThemeVariables({
-                dark: true,
-              }),
-              "@primary-color": "#4834d4",
+    plugins: [
+        {
+            plugin: CracoLessPlugin,
+            options: {
+                lessLoaderOptions: {
+                    lessOptions: {
+                        javascriptEnabled: true,
+                        modifyVars: {
+                            ...getThemeVariables({
+                                dark: false,
+                            }),
+                            '@primary-color': '#4834d4',
+                        },
+                    },
+                },
             },
-          },
         },
-      },
-    },
-  ],
-};
+    ],
+}
---
+++ frontend/src/utils/color.js
@@ -0,0 +1,3 @@
+export const blockText = '#4B525D'
+export const headerText = '#222222'
+export const defaultText = '#222222'
