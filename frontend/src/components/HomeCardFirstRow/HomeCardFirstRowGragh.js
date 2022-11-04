909074b4ce2d6d48731e50158b990a6687a04afc
cothi <jiungdev@gmail.com>
2022-11-04 18:06:21 +0900

    refac: HomeCardFirstRow component of Home

---
+++ frontend/src/components/HomeCardFirstRow/HomeCardFirstRowGragh.js
@@ -0,0 +1,108 @@
+import styled from 'styled-components'
+import { buttonColor, defaultText } from '../../utils/color'
+import React, { useEffect, useState } from 'react'
+import { Area } from '@ant-design/plots'
+
+const CardGraph = styled.div`
+    width: 60%;
+    height: 240px;
+`
+const WrapGraph = styled.div`
+    width: 100%;
+    height: 100%;
+    padding: 10px 10px 0 10px;
+`
+const GraphHeader = styled.div`
+    width: 100%;
+    height: 15%;
+    display: flex;
+    justify-content: flex-end;
+`
+const GraphButtonWrap = styled.div`
+    width: 40%;
+    border-radius: 10px;
+    background-color: #f5f5f5;
+    padding: 4px;
+    display: flex;
+    justify-content: space-between;
+`
+const GraphButton = styled.div`
+    background-color: ${(props) => (props.off ? '#f5f5f5' : `${buttonColor}`)};
+    color: ${(props) => (props.off ? `${defaultText}` : '#f5f5f5')};
+    border-radius: 10px;
+    width: 45%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+`
+
+const GraphContent = styled.div`
+    width: 100%;
+    height: 85%;
+`
+
+const HomeCardFirstRowGraph = () => {
+    const [data, setData] = useState([])
+    const [toggle, setToggle] = useState(false)
+
+    useEffect(() => {
+        asyncFetch()
+    }, [])
+
+    const asyncFetch = () => {
+        fetch(
+            'https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json'
+        )
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => {
+                console.log('fetch data failed', error)
+            })
+    }
+    const config = {
+        data,
+
+        xField: 'Date',
+        yField: 'scales',
+        line: { color: '#9c6cff' },
+        xAxis: {
+            range: [0, 1],
+            tickCount: 5,
+        },
+        yAxis: false,
+        areaStyle: () => {
+            return {
+                fill: 'l(270) 0:#ffffff 0.5:#9f86d1 1:#9c6cff',
+            }
+        },
+    }
+    return (
+        <CardGraph>
+            <WrapGraph>
+                <GraphHeader>
+                    <GraphButtonWrap>
+                        <GraphButton
+                            off={toggle}
+                            onClick={() => setToggle(!toggle)}
+                        >
+                            Price
+                        </GraphButton>
+                        <GraphButton
+                            off={!toggle}
+                            onClick={() => setToggle(!toggle)}
+                        >
+                            Volume
+                        </GraphButton>
+                    </GraphButtonWrap>
+                </GraphHeader>
+                <GraphContent>
+                    <Area style={{ height: '100%' }} {...config} />
+                </GraphContent>
+            </WrapGraph>
+        </CardGraph>
+    )
+}
+
+export default React.memo(HomeCardFirstRowGraph)
--- frontend/src/components/HomeSecodeRow/index.js
+++ frontend/src/components/HomeSecodeRow/index.js
@@ -25,36 +25,64 @@ export default function HomeCardSecondRow({
     return (
         <CardSecondRowRoot>
             <CardSecondRowCol
-                icon1={<Icon icon="clarity:blocks-group-solid" width="32" />}
+                icon1={
+                    <Icon
+                        icon="clarity:blocks-group-solid"
+                        color="#7a7979"
+                        width="32"
+                    />
+                }
                 header1={'Height'}
                 data1={height}
-                icon2={<Icon icon="uil:transaction" width="32" />}
+                icon2={
+                    <Icon icon="uil:transaction" color="#7a7979" width="32" />
+                }
                 header2={'Transactions'}
                 data2={transaction}
             />
             <CardSecondRowCol
-                icon1={<GiToken size={24} />}
+                icon1={<GiToken size={24} color="#7a7979" />}
                 header1={'Bonded Token'}
                 data1={bondedToken}
                 icon2={
-                    <Icon icon="mdi:google-circles-communities" width="32" />
+                    <Icon
+                        icon="mdi:google-circles-communities"
+                        color="#7a7979"
+                        width="32"
+                    />
                 }
                 header2={'Community Pool'}
                 data2={communnityPool}
             />
             <CardSecondRowCol
-                icon1={<Icon icon="octicon:graph-16" width="32" />}
+                icon1={
+                    <Icon icon="octicon:graph-16" color="#7a7979" width="32" />
+                }
                 header1={'Inflation'}
                 data1={inflation}
-                icon2={<Icon icon="iconoir:percentage-square" width={32} />}
+                icon2={
+                    <Icon
+                        icon="iconoir:percentage-square"
+                        color="#7a7979"
+                        width={32}
+                    />
+                }
                 header2={'Staking APR'}
                 data2={stakingApr}
             />
             <CardSecondRowCol
-                icon1={<Icon icon="clarity:blocks-group-solid" width="32" />}
+                icon1={
+                    <Icon
+                        icon="clarity:blocks-group-solid"
+                        color="#7a7979"
+                        width="32"
+                    />
+                }
                 header1={'Staking APR'}
                 data1={'1'}
-                icon2={<Icon icon="uil:transaction" width="32" />}
+                icon2={
+                    <Icon icon="uil:transaction" color="#7a7979" width="32" />
+                }
                 header2={'Transactions'}
                 data2={'2'}
             />
