59021f1fdca7b0456be5357d9df6384cc96c8d4c
cothi <jiungdev@gmail.com>
2022-11-06 19:18:05 +0900

    fix: change dashboard style and add icon

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,39 +1,80 @@
-import React from "react";
-import { Layout, Menu, Input } from "antd";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Layout, Menu, Input } from 'antd'
+import { Link } from 'react-router-dom'
+import { Icon } from '@iconify/react'
+import styled from 'styled-components'
 
-const { Header } = Layout;
-const { Search } = Input;
+const { Header } = Layout
+const { Search } = Input
+
+const StyledLink = styled(Link)`
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+`
 
 const Navbar = () => {
-  return (
-    <Header
-      style={{
-        position: "fixed",
-        zIndex: 1,
-        width: "100%",
-        display: "inline-flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      {/* <div>Logo</div> */}
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        style={{ width: 500 }}
-        items={[
-          { key: "DASHBOARD", label: <Link to="/">DASHBOARD</Link> },
-          {
-            key: "VALIDATORS",
-            label: <Link to="/validators">VALIDATORS</Link>,
-          },
-          { key: "BLOCKS", label: <Link to="/blocks">BLOCKS</Link> },
-        ]}
-      />
-      <Search style={{ width: 300 }} />
-    </Header>
-  );
-};
+    return (
+        <>
+            <Header
+                style={{
+                    backgroundColor: '#390368',
+                    position: 'fixed',
+                    zIndex: 1,
+                    width: '100%',
+                    display: 'inline-flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}
+            >
+                {/* <div>Logo</div> */}
+                <Menu
+                    theme="light"
+                    mode="horizontal"
+                    style={{ width: 400 }}
+                    items={[
+                        {
+                            key: 'DASHBOARD',
+                            label: (
+                                <StyledLink to="/">
+                                    <Icon
+                                        icon="fluent:board-split-28-regular"
+                                        style={{ marginRight: '5px' }}
+                                    />
+                                    DASHBOARD
+                                </StyledLink>
+                            ),
+                        },
+                        {
+                            key: 'VALIDATORS',
+                            label: (
+                                <StyledLink to="/validators">
+                                    <Icon
+                                        icon="akar-icons:people-group"
+                                        style={{ marginRight: '5px' }}
+                                    />
+                                    VALIDATORS
+                                </StyledLink>
+                            ),
+                        },
+                        {
+                            key: 'BLOCKS',
+                            label: (
+                                <StyledLink to="/blocks">
+                                    <Icon
+                                        icon="eos-icons:blockchain"
+                                        style={{ marginRight: '5px' }}
+                                    />
+                                    BLOCKS
+                                </StyledLink>
+                            ),
+                        },
+                    ]}
+                />
+                <Search style={{ width: 300 }} />
+            </Header>
+        </>
+    )
+}
 
-export default Navbar;
+export default Navbar
