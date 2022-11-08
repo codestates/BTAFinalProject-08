217958c47dba42c64911dc1a8b5a6d184a0965f0
LAPTOP-7G174PE1\이진우 <jinwoo0451@naver.com>
2022-11-08 14:48:53 +0900

    style: formatting

--- Daemon/modules/demonModules.js
+++ Daemon/modules/demonModules.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 const env = process.env;
-const {SigningStargateClient} = require('@cosmjs/stargate');
+const { SigningStargateClient } = require('@cosmjs/stargate');
 const endPoint = env.END_POINT // 노드 주소
-const {Block} = require("../models")
+const { Block } = require("../models")
 
 async function getCurrentHeight() {
     const signingClient = await SigningStargateClient.connect(endPoint);
@@ -10,18 +10,17 @@ async function getCurrentHeight() {
     return height;
 }
 
-
 async function extractBlocksInfoFromMinHeightToMaxHeight(minHeight, maxHeight) {
     if (typeof minHeight !== "number" || typeof maxHeight !== "number") null;
     if ((maxHeight - minHeight) >= 20) null;
     const blocks = await axios.get(env.END_POINT + "blockchain?minHeight=" + minHeight + "&maxHeight=" + maxHeight);
     if (!blocks || !blocks.data) return null;
-    const {result: {last_height: lastHeight, block_metas: blocksInfo}} = blocks.data;
+    const { result: { last_height: lastHeight, block_metas: blocksInfo } } = blocks.data;
     let result = [];
     blocksInfo.forEach(blockInfo => {
         const {
-            block_id: {hash},
-            header: {chain_id: chainId, height, time, proposer_address: proposerAddress},
+            block_id: { hash },
+            header: { chain_id: chainId, height, time, proposer_address: proposerAddress },
             num_txs: numOfTxs
         } = blockInfo;
         const extractedBlockInfo = {
@@ -53,7 +52,7 @@ async function pushBlock() {
                     numOfTx: i.numOfTxs,
                     proposerAddress: i.proposerAddress
                 });
-                console.log(i.height+"번째 생성완료");
+                console.log(i.height + "번째 생성완료");
             }
             console.log("created " + (db + 1) + " to " + (db + 20))
         } else if (Number(db) === height) {
@@ -70,13 +69,11 @@ async function pushBlock() {
                     numOfTx: i.numOfTxs,
                     proposerAddress: i.proposerAddress
                 });
-                console.log(i.height+"번째 생성완료");
+                console.log(i.height + "번째 생성완료");
             }
             console.log("created " + (db + 1) + " to " + height)
         }
     }, 5000);
-
-
 }
 
-module.exports = {extractBlocksInfoFromMinHeightToMaxHeight, getCurrentHeight, pushBlock};
+module.exports = { extractBlocksInfoFromMinHeightToMaxHeight, getCurrentHeight, pushBlock };
