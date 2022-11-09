806de2a744a57389740be1ccdcb0036a3981a8d8
LAPTOP-7G174PE1\이진우 <jinwoo0451@naver.com>
2022-11-10 05:31:18 +0900

    fix: remove moniker in validator info

--- Backend/modules/utils.js
+++ Backend/modules/utils.js
@@ -4,7 +4,6 @@ const { encodeBech32Pubkey, decodeBech32Pubkey } = require("@cosmjs/launchpad");
 
 
 const Cothi = Object.freeze({
-    moniker: "cothi",
     pubKey: "hSx2cwvzoi3dhRDnOpTZO6icB0ZImX+T5kZHWLSmD9U=",
     operatorAddress: "osmovaloper1w6k4anx2juthw8ka0zyxej26wvdlccwq09x0c6",
     address: "osmo1w6k4anx2juthw8ka0zyxej26wvdlccwq4jwv0a",
@@ -12,22 +11,26 @@ const Cothi = Object.freeze({
 });
 
 const Calvin = Object.freeze({
-    moniker: "calvin",
     pubKey: "/3802T/tAnDPqNbyNHQQLhMDM0dZDZ1yWFCEesAcM6Q=",
     operatorAddress: "osmovaloper1erhzfvsfhue4tz6cz9czrgucecxp3uddf3dds4",
     address: "osmo1erhzfvsfhue4tz6cz9czrgucecxp3uddnx9w8j",
     hex: "433AFC12BFA4E7A537629EB9580F8EA7ADE23FDC"
 });
 
-const Codemonkeyshin = Object.freeze({
-    moniker: "codemonkeyshin",
+const CodemonkeyShin = Object.freeze({
     pubKey: "cVTM6Vw4f2uSpQKQNCyf5Fmj6N76E19MbbEVUtQPcT0=",
     operatorAddress: "osmovaloper1mhfgfenrp88d2p5dttyw59x8frfk7u9lujg49y",
     address: "osmo1mhfgfenrp88d2p5dttyw59x8frfk7u9lx9qkjr",
     hex: "5BDA2D761F5CE86F586870477937D42847F12F11"
 });
 
-const AddressToMoniker = Object.freeze({
+const MonikerToAddressInfo = Object.freeze({
+    calvin: Calvin,
+    cothi: Cothi,
+    codemonkeyshin: CodemonkeyShin
+});
+
+const HexAddressToMoniker = Object.freeze({
     "5BDA2D761F5CE86F586870477937D42847F12F11": "codemonkeyshin",
     "FBA3AEF9CB504C662AED5C677EEF0C810E5BEEEE": "cothi",
     "433AFC12BFA4E7A537629EB9580F8EA7ADE23FDC": "calvin",
@@ -49,4 +52,4 @@ const TxTypes = Object.freeze({
     WITHDRAW_DELEGATOR_REWARD: "WithdrawDelegationReward",
 });
 
-module.exports = { getAddressFromPubKey, TxTypes, Cothi, Codemonkeyshin, Calvin, AddressToMoniker };
+module.exports = { getAddressFromPubKey, MonikerToAddressInfo, TxTypes, HexAddressToMoniker };
