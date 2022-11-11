08b1b4dde340fe7c1a1eee6556e738ff856d7491
cothi <jiungdev@gmail.com>
2022-11-12 02:04:02 +0900

    feat: wallet active btn click event

--- wallet/src/pages/Background/index.js
+++ wallet/src/pages/Background/index.js
@@ -1,2 +1,61 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+console.log('This is the background page.');
+console.log('Put the background scripts here.');
+
+let winid;
+// After the tab has been created, open a window to inject the tab
+async function openPopup() {
+  if (winid !== undefined) {
+    try {
+      const window = await chrome.windows.get(winid, { populate: true });
+      console.log(window, 'window!!!');
+      if (window?.tabs?.lengh) {
+        const tabs = window.tabs[0];
+        chrome.windows.update(tabs.id, {
+          active: true,
+          url: '/popup.html',
+        });
+      }
+
+      console.log('ge');
+    } catch {
+      chrome.windows.create(
+        {
+          url: '/popup.html',
+          type: 'popup',
+          width: 430,
+          height: 500,
+          top: 400,
+          left: 400,
+        },
+        function (win) {
+          winid = win.id;
+        }
+      );
+    }
+  } else {
+    chrome.windows.create(
+      {
+        url: '/popup.html',
+        type: 'popup',
+        width: 430,
+        height: 500,
+        top: 400,
+        left: 400,
+      },
+      function (win) {
+        winid = win.id;
+      }
+    );
+  }
+}
+chrome.runtime.onMessage.addListener(async function (
+  request,
+  sender,
+  sendResponse
+) {
+  openPopup();
+  sendResponse('got it!', request.greeting, sender);
+});
--- wallet/src/pages/Content/index.js
+++ wallet/src/pages/Content/index.js
@@ -11,4 +11,19 @@ chrome.storage.sync.get((item) => console.log({ item }));
 container.insertBefore(scriptElement, container.children[0]);
 scriptElement.remove();
 
+if (document.readyState !== 'complete') {
+  window.addEventListener('load', afterWindowLoaded);
+} else {
+  afterWindowLoaded();
+}
+
+function afterWindowLoaded() {
+  //Everything that needs to happen after the window is fully loaded.
+  document.getElementById('gege').addEventListener('click', function () {
+    chrome.runtime.sendMessage({ greeting: 'hello' }, function (response) {
+      console.log(response);
+    });
+  });
+}
+
 printLine("Using the 'printLine' function from the Print Module");
