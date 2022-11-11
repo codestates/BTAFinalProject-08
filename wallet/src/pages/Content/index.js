5786cf56032b6b04728d964a43d340cbcafd6867
younghunban <younghunban@yeonghun-MacBookPro-14.local>
2022-11-11 16:38:39 +0900

    feat: 익스플로러 크롬 스토리지 접근 처리

--- wallet/src/pages/Content/index.js
+++ wallet/src/pages/Content/index.js
@@ -1,6 +1,14 @@
 import { printLine } from './modules/print';
 
-console.log('Content script works!');
+console.log('Content script works!, 11');
 console.log('Must reload extension for modifications to take effect.');
+
+const container = document.head || document.documentElement;
+const scriptElement = document.createElement('script');
+scriptElement.src = chrome.runtime.getURL('injectScript.bundle.js');
+scriptElement.type = 'text/javascript';
+chrome.storage.sync.get((item) => console.log({ item }));
+container.insertBefore(scriptElement, container.children[0]);
+scriptElement.remove();
 
 printLine("Using the 'printLine' function from the Print Module");
