8b61049f91564b49ce9565ccb5370c5e0d72a61b
younghunban <younghunban@yeonghun-MacBookPro-14.local>
2022-11-13 19:42:44 +0900

    feat: 보팅, 디파짓 구현

--- wallet/src/pages/Popup/routes/ProposalDetail.jsx
+++ wallet/src/pages/Popup/routes/ProposalDetail.jsx
@@ -1,14 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getProposalDetail } from '../utils/api';
-import { Card, Spin, Descriptions, Button, Tag } from 'antd';
+import {
+  Card,
+  Spin,
+  Descriptions,
+  Button,
+  Tag,
+  Input,
+  message,
+  Select,
+} from 'antd';
 import { isDepositPeriod } from './Voting';
 import moment from 'moment';
+import * as wallet from '../utils/wallet';
+import { useGetSignerInfo } from '../hooks/useGetSignerInfo';
 
 const ProposalDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [proposal, setProposal] = useState();
+  const [amount, setAmount] = useState('');
+  const [voteOpt, setVoteOpt] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    signerInfo: { address, signingClient },
+  } = useGetSignerInfo();
+
+  const defaultBtnOptions = {
+    htmlType: 'submit',
+    type: 'primary',
+    loading: isLoading,
+  };
 
   useEffect(() => {
     const fetchProposal = async () => {
@@ -17,6 +40,43 @@ const ProposalDetail = () => {
     };
     fetchProposal();
   }, [id]);
+
+  const onClickDeposit = async () => {
+    try {
+      setIsLoading(true);
+      const res = await wallet.utils.sendDeposit(
+        id,
+        signingClient,
+        address,
+        amount
+      );
+      console.log(res, 'res');
+      message.success('디파짓 완료');
+      navigate(-1);
+      setIsLoading(false);
+    } catch (e) {
+      message.error(e.message);
+      navigate(-1);
+      setIsLoading(false);
+    }
+  };
+
+  const onClickVoting = async () => {
+    try {
+      setIsLoading(true);
+      const res = await wallet.utils.sendVoting(
+        id,
+        signingClient,
+        address,
+        voteOpt
+      );
+      console.log(res, 'res');
+      message.success('보팅 완료');
+      navigate(-1);
+    } catch (e) {
+      message.error(e.message);
+    }
+  };
 
   return proposal ? (
     <Card title={proposal.content.title}>
@@ -35,24 +95,62 @@ const ProposalDetail = () => {
           {moment(proposal.submit_time).format('YYYY-MM-DD HH:m:s')}
         </Descriptions.Item>
         <Descriptions.Item label="Voting Start">
-          {proposal.voting_start_time}
+          {moment(proposal.voting_start_time).format('YYYY-MM-DD HH:m:s')}
         </Descriptions.Item>
         <Descriptions.Item label="Voting End">
-          {proposal.voting_end_time}
+          {moment(proposal.voting_end_time).format('YYYY-MM-DD HH:m:s')}
         </Descriptions.Item>
         <Descriptions.Item label="Submit Time">
-          {proposal.submit_time}
+          {moment(proposal.submit_time).format('YYYY-MM-DD HH:m:s')}
         </Descriptions.Item>
         <Descriptions.Item label="Deposit End">
-          {proposal.deposit_end_time}
+          {moment(proposal.deposit_end_time).format('YYYY-MM-DD HH:m:s')}
         </Descriptions.Item>
         <Descriptions.Item label="Detail">
           {proposal.content.description}
         </Descriptions.Item>
       </Descriptions>
-      <Button type="primary">
-        {isDepositPeriod(proposal.status) ? '보팅' : '디파짓'}
-      </Button>
+      {isDepositPeriod(proposal.status) ? (
+        <Input
+          placeholder="amount"
+          size="large"
+          style={{ marginBottom: 14 }}
+          suffix="uosmo"
+          onChange={(e) => setAmount(e.target.value)}
+        />
+      ) : (
+        <div>
+          <Select
+            size="large"
+            defaultValue={'옵션 선택'}
+            style={{ width: 200, marginBottom: 14 }}
+            onChange={(value) => setVoteOpt(value)}
+            options={[
+              { value: 'yes', label: 'yes' },
+              { value: 'no', label: 'no' },
+              { value: 'noWithVeto', label: 'noWithVeto' },
+              { value: 'abstrain', label: 'abstrain' },
+            ]}
+          />
+        </div>
+      )}
+      {isDepositPeriod(proposal.status) ? (
+        <Button
+          {...defaultBtnOptions}
+          onClick={onClickDeposit}
+          disabled={amount.length === 0}
+        >
+          디파짓{console.log(isDepositPeriod(proposal.status))}
+        </Button>
+      ) : (
+        <Button
+          {...defaultBtnOptions}
+          onClick={onClickVoting}
+          disabled={voteOpt === null}
+        >
+          보팅
+        </Button>
+      )}
       <Button className="cancel-btn" onClick={() => navigate(-1)}>
         취소
       </Button>
